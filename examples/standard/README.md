7b9baa69c24a45e3653afa64fa786e7fa9fc0b32
Craig Thacker <craigthackerx@gmail.com>
2023-02-27 23:09:52 +0000

    Update module

--- examples/standard/README.md
+++ examples/standard/README.md
@@ -1,28 +1,4 @@
-```hcl
-module "rg" {
-  source = "registry.terraform.io/libre-devops/rg/azurerm"
-
-  rg_name  = "rg-${var.short}-${var.loc}-${terraform.workspace}-build" // rg-ldo-euw-dev-build
-  location = local.location                                            // compares var.loc with the var.regions var to match a long-hand name, in this case, "euw", so "westeurope"
-  tags     = local.tags
-
-  #  lock_level = "CanNotDelete" // Do not set this value to skip lock
-}
-
-
-module "cosmosdb" {
-  source = "registry.terraform.io/libre-devops/cosmosdb/azurerm"
-
-  rg_name  = module.rg.rg_name
-  location = module.rg.rg_location
-  tags     = module.rg.rg_tags
-
-  cosmosdb_name = "cosmos-test"
-
-  cosmosdb_account_properties = {}
-
-}
-
+```
 
 ## Requirements
 
---
+++ examples/standard/test2.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+
+
+function tfrel ()
+{
+    print_success() {
+        lightcyan='\033[1;36m'
+        nocolor='\033[0m'
+        echo -e "${lightcyan}$1${nocolor}"
+    }
+
+    print_error() {
+        lightred='\033[1;31m'
+        nocolor='\033[0m'
+        echo -e "${lightred}$1${nocolor}"
+    }
+
+    print_alert() {
+        yellow='\033[1;33m'
+        nocolor='\033[0m'
+        echo -e "${yellow}$1${nocolor}"
+    }
+
+    local curdir=$(basename $(pwd));
+    alias stfi='curl https://raw.githubusercontent.com/libre-devops/utils/dev/scripts/terraform/tf-sort.sh | bash -s -- input.tf input.tf'
+    alias stfo='curl https://raw.githubusercontent.com/libre-devops/utils/dev/scripts/terraform/tf-sort.sh | bash -s -- output.tf output.tf'
+
+    if ls *.tf &> /dev/null; then
+        rm -rf README.md
+        stfi
+        stfo
+        if [ -f build.tf ];  then
+            echo "" > README.md && echo '```hcl' | cat - build.tf |  echo '```' | cat - README.md > temp.md && mv temp.md README.md
+
+        elif [ -f main.tf ]; then
+            echo "" > README.md && echo '```hcl' | cat - main.tf |  echo '```' | cat - README.md > temp.md && mv temp.md README.md
+
+        else
+            print_alert "Not a build directory, no build.tf found"
+        fi
+        terraform-docs markdown . >> README.md
+        git add --all
+        git commit -m "Update module"
+        git push
+        git tag 1.0.0 --force
+        git push --tags --force
+
+    else
+        print_error "Error: No terraform files found within ${curdir}";
+    fi
+}
+
+echo "Appending functions to .bashrc"
+echo "" >> ~/.bashrc
+echo "# Define tfrel function" >> ~/.bashrc
+declare -f tfrel >> ~/.bashrc
